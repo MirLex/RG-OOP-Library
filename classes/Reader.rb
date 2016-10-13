cae76d7686f9949811c10a399dfab2912a3887c0
Mirshavka Aleksandr <mirshavka.alexander@gmail.com>
2016-10-13 20:36:19 +0300

    create Reader class

---
+++ classes/Reader.rb
@@ -0,0 +1,12 @@
+class Reader < Person
+	attr_accessor :biography, :email, :city, :street, :house
+
+	def initialize(name, email, city, street, house)
+		@email = email
+		@city = city
+		@street = street
+		@house = house
+		super(name)
+	end
+
+end
