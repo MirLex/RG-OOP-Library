6e5e3aa3d4f18f6031d91f180aff26dcbb81a2ab
Mirshavka Aleksandr <mirshavka.alexander@gmail.com>
2016-10-14 10:26:16 +0300

    add export/import

--- classes/Library.rb
+++ classes/Library.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 class Library
 	attr_reader :books ,:orders, :readers, :authors
 
@@ -7,6 +9,24 @@ class Library
 		@orders = orders
 		@readers = readers
 		@authors = authors
+	end
+
+	def export(file_path, method='YAML')
+		if method.downcase == 'yaml'
+			File.open(file_path +'.yaml','w') {|f| f.write(YAML.dump(self))}
+		end
+		if method.downcase == 'serialize'
+			File.open(file_path +'.dat','w') {|f| f.write(Marshal.dump(self))}
+		end	
+	end
+
+	def self.import(file_path, method='YAML')
+		if method.downcase == 'yaml'
+			return YAML.load(File.read(file_path + '.yaml'))
+		end
+		if method.downcase == 'serialize'
+			return Marshal.load(File.read(file_path + '.dat'))		
+		end	
 	end
 
 end
