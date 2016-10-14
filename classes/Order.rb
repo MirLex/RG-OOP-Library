20239625ef3058add9616be879e832fd82877139
Mirshavka Aleksandr <mirshavka.alexander@gmail.com>
2016-10-14 11:38:30 +0300

    Order time generated when creating

--- classes/Order.rb
+++ classes/Order.rb
@@ -1,8 +1,8 @@
 class Order
 	attr_reader :book ,:reader, :date
 
-	def initialize(book, reader, date)
-		@date = date
+	def initialize(book, reader)
+		@date = Time.now.strftime("%d/%m/%Y %H:%M")
 
 		#TODO check class
 		@book = book
