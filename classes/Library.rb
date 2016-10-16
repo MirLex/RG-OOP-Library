529a2fd34b9fc88a5da3be2d1287140edaf1d336
Mirshavka Aleksandr <mirshavka.alexander@gmail.com>
2016-10-16 13:42:31 +0300

    Refactor Library

--- classes/Library.rb
+++ classes/Library.rb
@@ -9,7 +9,7 @@ class Library
 		@books = books
 		@orders = orders
 		@readers = readers
-		@authors = authors
+		@authors = authors	
 	end
 
 	def export(file_path, method='YAML')
@@ -37,39 +37,25 @@ class Library
 
 	def getStats
 		putSeparator('The most popular book:')
-		puts mostPopularBook
+		puts getBest(:book,1)
 		putSeparator('Who often takes the book:')
-		puts bestReader
+		puts getBest(:reader,1)
 		putSeparator('How many people ordered one of the three most popular books:')
 		puts readersWhoOrderPopularBook.count
 	end
 
 	private
-	def mostPopularBook(books_count = 1)
-		#v1 ?
-		# booksRate = Hash[ self.orders.map { |v| [ v.book, condition with v] } ]
-		
-		#v2 done !
-		# booksRate = Hash.new{0}
-		# self.orders.each{ |order| booksRate[order.book] += 1}
-		# (booksRate.select {|k,v| k if v == booksRate.values.max }).keys
-		
-		#v3 count books are paremetr! for fun countReadersWhoOrderPopularBook
-		booksRate = Hash.new{0}
-		self.orders.each{ |order| booksRate[order.book] += 1}
-		booksRate.sort_by {|book,orders_count| orders_count}.reverse.to_h.keys[0...books_count]
-	end
 
-	def bestReader(readers_count = 1)
-		#TODO the same fun as mostPopularBook // create lambda or prok ?
-		readersRate = Hash.new{0}
-		self.orders.each{ |order| readersRate[order.reader] +=1}
-		readersRate.sort_by{|reader,orders_count| orders_count}.reverse.to_h.keys[0...readers_count]
+	def getBest(obj,count)
+		lambda { |obj,count|
+				result = Hash.new{0}
+				self.orders.each{ |order| result[order.send(obj)] +=1}
+				result.sort_by{|obj,orders_count| orders_count}.reverse.to_h.keys[0...count]
+		}.call(obj,count)
 	end
 
 	def readersWhoOrderPopularBook(books_count = 3)
-		#TODO One line code ?
-		mostPopularBooks = mostPopularBook(3)
+		mostPopularBooks = getBest(:book,3)
 		self.orders.group_by {|order| order.book}
 			.map{|book,order| order if mostPopularBooks.include?(book)}.compact
 			.flatten.uniq{|order| order.reader}
