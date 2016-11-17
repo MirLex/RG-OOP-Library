d308d413113655a5c70de0bc6d69105dbf4ce665
Mirshavka Aleksandr <mirshavka.alexander@gmail.com>
2016-11-17 11:51:41 +0200

    Refactor DataTransfer

--- modules/dataTransfer.rb
+++ modules/dataTransfer.rb
@@ -2,10 +2,11 @@ module DataTransfer
   module ClassMethods
     def import(file_path, method = 'YAML')
       if File.file?(file_path)
-        obj = YAML.load(File.read(file_path)) if method.casecmp('yaml').zero?
-        if method.casecmp('serialize').zero?
-          obj = Marshal.load(File.read(file_path))
-        end
+        obj = if method.casecmp('serialize').zero?
+                Marshal.load(File.read(file_path))
+              else
+                YAML.load(File.read(file_path))
+              end
       end
 
       obj.class == self ? obj : (raise ArgumentError, 'Incorrect file')
@@ -14,11 +15,10 @@ module DataTransfer
 
   module InstanceMethods
     def export(file_path, method = 'YAML')
-      if method.casecmp('yaml').zero?
-        File.open(file_path, 'w') { |f| f.write(YAML.dump(self)) }
-      end
       if method.casecmp('serialize').zero?
         File.open(file_path, 'w') { |f| f.write(Marshal.dump(self)) }
+      else
+        File.open(file_path, 'w') { |f| f.write(YAML.dump(self)) }
       end
       file_path
     end
